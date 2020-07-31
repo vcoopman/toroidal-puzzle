ad0de75443f0b3a49bae1f15c153b73b77045baf
Leonardo Aravena <ld.aravena@gmail.com>
2020-07-30 20:00:19 -0400

    Imagen para crear los cuados

--- app/src/main/java/com/curso/toroidal_puzzle/Cuadro.kt
+++ app/src/main/java/com/curso/toroidal_puzzle/Cuadro.kt
@@ -1,8 +1,9 @@
 package com.curso.toroidal_puzzle
 
+import android.graphics.Bitmap
 import android.widget.ImageView
 
-class Cuadro(imageResource: Int, idVista: ImageView){
+class Cuadro(imageResource: Bitmap, idVista: ImageView){
     val imageResource = imageResource
     val idVista = idVista
 }
